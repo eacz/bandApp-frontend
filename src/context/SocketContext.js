e82a8e3c2475952c2285ab16a366e5a6d6751dfd
Esteban Canteros <estebanacanteros@gmail.com>
2021-09-11 23:46:26 -0300

    enviroment variables config

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -5,7 +5,7 @@ export const SocketContext = createContext()
 
 const SocketProvider = ({children}) => {
 
-  const { online, socket } = useSocket('http://localhost:4000')
+  const { online, socket } = useSocket(process.env.REACT_APP_BACKEND_URL)
 
   return (
     <SocketContext.Provider
